ee5eaeeb2a455a40dddf7e853737488a6121da51
Micha Buchholz <mail@lean-stack.rocks>
2025-02-28 09:52:25 +0100

    Icon added to title

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,53 +1,66 @@
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarFooter,
-    SidebarGroup,
-    SidebarHeader,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
-    SidebarRail,
-    SidebarSeparator,
-    useSidebar,
-  } from "@/components/ui/sidebar"
-import { Link } from "@tanstack/react-router"
-import { FoldersIcon, HomeIcon, SearchIcon } from "lucide-react"
-  
-  export function AppSidebar() {
-    const { setOpenMobile } = useSidebar();
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+  SidebarSeparator,
+  useSidebar,
+} from '@/components/ui/sidebar';
+import { Link } from '@tanstack/react-router';
+import {
+  FoldersIcon,
+  HomeIcon,
+  RefrigeratorIcon,
+  SearchIcon,
+} from 'lucide-react';
 
-    return (
-      <Sidebar >
-        <SidebarHeader>
-            <span className="text-xl font-medium">Fridge</span>
-        </SidebarHeader>
-        <SidebarSeparator />
-        <SidebarContent>
-          <SidebarGroup>
-            <SidebarMenu>
-                <SidebarMenuItem>
-                    <SidebarMenuButton asChild onClick={() => setOpenMobile(false)}>
-                        <Link to="/"><HomeIcon /> Home</Link>
-                    </SidebarMenuButton>
-                </SidebarMenuItem>
-                <SidebarMenuItem>
-                    <SidebarMenuButton asChild onClick={() => setOpenMobile(false)}>
-                        <Link to="/category"><FoldersIcon /> Kategorie</Link>
-                    </SidebarMenuButton>
-                </SidebarMenuItem>
-                <SidebarMenuItem>
-                    <SidebarMenuButton asChild onClick={() => setOpenMobile(false)}>
-                        <Link to="/filter"><SearchIcon /> Filter</Link>
-                    </SidebarMenuButton>
-                </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroup>
-          <SidebarGroup />
-        </SidebarContent>
-        <SidebarFooter />
-        <SidebarRail />
-      </Sidebar>
-    )
-  }
-  
+export function AppSidebar() {
+  const { setOpenMobile } = useSidebar();
+
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <div className="flex items-center gap-x-2">
+          <RefrigeratorIcon />
+          <span className="text-xl font-medium">Fridge</span>
+        </div>
+      </SidebarHeader>
+      <SidebarSeparator />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild onClick={() => setOpenMobile(false)}>
+                <Link to="/">
+                  <HomeIcon /> Home
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild onClick={() => setOpenMobile(false)}>
+                <Link to="/category">
+                  <FoldersIcon /> Kategorie
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild onClick={() => setOpenMobile(false)}>
+                <Link to="/filter">
+                  <SearchIcon /> Filter
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+      <SidebarRail />
+    </Sidebar>
+  );
+}
